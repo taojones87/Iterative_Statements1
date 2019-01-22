2991ae8c5f166a91967165a20e55dbc6b4d601f3
taojones87@gmail.com <taojones87@gmail.com>
2019-01-22 07:50:03 -0500

    Version 2

--- Iterative_Statements/Program.cs
+++ Iterative_Statements/Program.cs
@@ -6,7 +6,31 @@ namespace Iterative_Statements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter an integer value between 1 and 100.");
+
+            try
+            {
+                // Gather the input value and convert to string variable.
+                string input = Console.ReadLine();
+                int value = int.Parse(input);
+
+                // Run the for loop for the iteration.
+
+                for (int i = 0; i <= value; i++)
+                {
+                    Console.WriteLine("You have entered " + input + ". This is the current integer value in the loop: " + i.ToString());
+                }
+            }
+            catch
+            {
+                // Remind user of parameters and provide program close instructions.
+                Console.WriteLine("Please run the program again and remember to enter an integer value between 1 and 100.");
+                Console.WriteLine("Press any key to exit the program...");
+                Console.ReadKey(true);
+            }
+            // Provide program close instructions.
+            Console.WriteLine("Press any key to exit the program...");
+            Console.ReadKey(true);
         }
     }
 }
