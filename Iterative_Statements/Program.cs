610926687a81c4dd71c18949d119e880178cecd7
taojones87@gmail.com <taojones87@gmail.com>
2019-01-24 17:40:56 -0500

    Version 3

--- Iterative_Statements/Program.cs
+++ Iterative_Statements/Program.cs
@@ -14,11 +14,24 @@ namespace Iterative_Statements
                 string input = Console.ReadLine();
                 int value = int.Parse(input);
 
-                // Run the for loop for the iteration.
-
-                for (int i = 1; i <= value; i++)
+                if (value > 0 && value <=100)
                 {
-                    Console.WriteLine("You have entered " + input + ". This is the current integer value in the loop: " + i.ToString());
+                    // Run the for loop for the iteration.
+
+                    for (int i = 1; i <= value; i++)
+                    {
+                        Console.WriteLine("You have entered " + input + ". This is the current integer value in the loop: " + i.ToString());
+                    }
+                    // Provide program close instructions.
+                    Console.WriteLine("Press any key to exit the program...");
+                    Console.ReadKey(true);
+                }
+                else
+                {
+                    // Remind user of parameters and provide program close instructions.
+                    Console.WriteLine("Please run the program again and remember to enter an integer value between 1 and 100.");
+                    Console.WriteLine("Press any key to exit the program...");
+                    Console.ReadKey(true);
                 }
             }
             catch
@@ -28,9 +41,6 @@ namespace Iterative_Statements
                 Console.WriteLine("Press any key to exit the program...");
                 Console.ReadKey(true);
             }
-            // Provide program close instructions.
-            Console.WriteLine("Press any key to exit the program...");
-            Console.ReadKey(true);
         }
     }
 }
